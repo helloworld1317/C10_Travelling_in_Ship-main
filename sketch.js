e662b399eca138cc8bf5215d98433ee2f9f509e1
helloworld1317 <87151984+helloworld1317@users.noreply.github.com>
2021-08-10 16:41:48 -0700

    c-10 travelling in a ship final

--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
 function preload(){
 
 seaImg=loadImage("sea.png");
-shipImg= loadAnimation("ship-1.png","ship-2.png");
+shipImg= loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 createCanvas(400,400);
-
+var sea=createSprite(200,200,400,400);
+sea.addImage(seaImg);
+sea.velocityX=-3
+if (sea.x<0){
+ sea.x=sea.width/2;
 }
+var ship= createSprite(200,260,100,100);
+  ship.addAnimation("ship",shipImg);
+ship.scale=0.3;
+} 
 
 function setup(){
-  var sea=createSprite(200,200,400,400);
-  sea.addImage(seaImg);
-  sea.velocityX=-3
-  if (sea.x<0){
-  
-  }
-  var ship= createSprite(200,260,100,100);
-    ship.addAnimation(shipImg);
-  ship.scale=0.3;
+
   
 }
 
